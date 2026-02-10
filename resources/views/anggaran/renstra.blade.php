daf1f72818826ef82adf49246ff973949354b76e
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-10 12:19:46 +0800

    mulai buat tabel

--- resources/views/anggaran/renstra.blade.php
+++ resources/views/anggaran/renstra.blade.php
@@ -4,214 +4,141 @@
 <div class="ui stackable grid">
     <div class="two wide left column">
         <div class="ui red secondary vertical pointing fluid menu">
-            <a class="active item inayah" data-tab="tab_renstra" tbl="renstra">
-                Renstra
+            <a class="active item" data-tab="tab_renstra" tbl="renstra">
+                <i class="clipboard list icon"></i> Renstra
             </a>
-            <a class="item inayah" data-tab="tab_renstra" tbl="tujuan_sasaran_renstra">
-                Tujuan dan Sasaran
+            <a class="item" data-tab="tab_renstra" tbl="tujuan_sasaran_renstra">
+                <i class="bullseye icon"></i> Tujuan dan Sasaran
             </a>
         </div>
     </div>
+
     <div class="fourteen wide stretched right column">
-        <h1 class="ui header">Rencana Strategis (Renstra) <div class="sub header">dokumen perencanaan
-                berorientasi
-                pada hasil yang ingin dicapai</div>
+        <h1 class="ui header">
+            Rencana Strategis (Renstra)
+            <div class="sub header">Dokumen perencanaan berorientasi hasil</div>
         </h1>
+
         <div class="ui hidden divider"></div>
-        <div class="ui stretched stackable five column grid">
-            <div class="column">
-                <div class="ui orange icon message goyang"><i class="book icon"></i>
-                    <div class="content">
-                        <div class="header">Total Anggaran</div>
-                        <p name="total-anggaran"></p>
-                    </div>
+
+        <!-- Statistik ringkas (diisi via JS atau AJAX nanti) -->
+        <div class="ui grid">
+            <div class="five wide column">
+                <div class="ui orange message">
+                    <div class="header">Total Anggaran</div>
+                    <p name="total-anggaran">Rp —</p>
                 </div>
             </div>
-            <div class="column">
-                <div class="ui icon yellow message goyang">
-                    <i class="chart icon" name="chart-realisasi-fisik-mini"></i>
-                    <div class="content">
-                        <div class="header">Jumlah Program</div>
-                        <p name="realisasi-fisik"></p>
-                    </div>
-                </div>
+            <!-- tambahkan kolom statistik lain sesuai kebutuhan -->
+        </div>
+
+        <div class="ui fluid container">
+            <div class="ui hidden divider"></div>
+
+            <div class="ui right floated basic icon buttons">
+                <button class="ui button" jns="add" tbl="renstra" data-tooltip="Tambah">
+                    <i class="plus icon"></i>
+                </button>
+                <button class="ui button" jns="import" tbl="renstra" data-tooltip="Import XLSX">
+                    <i class="upload icon"></i>
+                </button>
+                <button class="ui button" jns="download" tbl="renstra" data-tooltip="Download">
+                    <i class="download icon"></i>
+                </button>
             </div>
-            <div class="column">
-                <div class="ui olive icon message goyang"><i class="chart icon" name="chart-realisasi-keu-mini"></i>
-                    <div class="content">
-                        <div class="header">Jumlah Kegiatan</div>
-                        <p name="realisasi-keu"></p>
-                    </div>
-                </div>
-            </div>
-            <div class="column">
-                <div class="ui icon red message goyang"><i class="spinner loading icon"></i>
-                    <div class="content">
-                        <div class="header">Jumlah Sub Kegiatan</div>
-                        <p name="sisa-fisik"></p>
-                    </div>
-                </div>
-            </div>
-            <div class="column">
-                <div class="ui positive icon message goyang"><i class="spinner loading icon"></i>
-                    <div class="content">
-                        <div class="header">Sisa Keuangan</div>
-                        <p name="sisa-keu"></p>
-                    </div>
-                </div>
-            </div>
-            <div class="ui fluid container">
-                <div class="ui hidden divider"></div>
-                <div class="ui right floated basic icon buttons">
-                    <button class="ui button" name="flyout" data-tooltip="Tambah Data" data-position="bottom center"
-                        jns="add" tbl="renstra"><i class="plus icon"></i></button>
-                    <button class="ui button" name="flyout" jns="import" data-tooltip="Import XLSX"
-                        data-position="bottom center" tbl="renstra"><i class="upload icon"></i></button> <button
-                        class="ui button" data-tooltip="Download" data-position="bottom center" name="ungguh" jns="dok"
-                        tbl="renstra" type="submit"><i class="alternate download icon"></i></button>
-                </div>
-                <h3 class="ui dividing header"><i class="left align icon"></i>Tabel Dokumen</h3>
-                <div class="ui hidden divider"></div>
-                <div class="ui hidden divider"></div>
-                <div class="ui long scrolling fluid container">
-                    <table class="ui head foot stuck unstackable celled striped table insert">
-                        <thead>
-                            <tr class="center aligned">
-                                <th>Kode</th>
-                                <th>Program dan Kegiatan</th>
-                                <th>Satuan</th>
-                                <th>Indikator Kinerja</th>
-                                <th>Data Capaian Awal</th>
-                                <th class="collapsing">Jumlah</th>
-                                <th>Kondisi Kinerja Akhir Renstra</th>
-                                <th class="collapsing">AKSI</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr id_row="95">
-                                <td klm="kd_sub_keg">1</td>
-                                <td klm="uraian_prog_keg">URUSAN PEMERINTAHAN WAJIB YANG BERKAITAN DENGAN PELAYANAN
-                                    DASAR</td>
-                                <td klm="satuan">
-                                    <div contenteditable=""></div>
-                                </td>
-                                <td klm="indikator">
-                                    <div contenteditable=""></div>
-                                </td>
-                                <td klm="data_capaian_awal">
-                                    <div contenteditable="" rms="" onkeypress="return rumus(event);">0,00</div>
-                                </td>
-                                <td klm="kondisi_akhir">0,00</td>
-                                <td klm="jumlah">977.945.193.060,23</td>
-                                <td class="center aligned">
-                                    <div class="ui icon basic mini buttons"><button class="ui red button" name="del_row"
-                                            jns="edit" tbl="renstra" id_row="95"><i
-                                                class="trash alternate outline red icon"></i></button></div>
-                                </td>
-                            </tr>
-                            <tr id_row="94">
-                                <td klm="kd_sub_keg">1.3</td>
-                                <td klm="uraian_prog_keg">URUSAN PEMERINTAHAN BIDANG PEKERJAAN UMUMDAN PENATAAN RUANG
-                                </td>
-                                <td klm="satuan">
-                                    <div contenteditable=""></div>
-                                </td>
-                                <td klm="indikator">
-                                    <div contenteditable=""></div>
-                                </td>
-                                <td klm="data_capaian_awal">
-                                    <div contenteditable="" rms="" onkeypress="return rumus(event);">0,00</div>
-                                </td>
-                                <td klm="kondisi_akhir">0,00</td>
-                                <td klm="jumlah">977.945.193.060,23</td>
-                                <td class="center aligned">
-                                    <div class="ui icon basic mini buttons"><button class="ui red button" name="del_row"
-                                            jns="edit" tbl="renstra" id_row="94"><i
-                                                class="trash alternate outline red icon"></i></button></div>
-                                </td>
-                            </tr>
-                            <tr id_row="132">
-                                <td klm="kd_sub_keg">1.3.02</td>
-                                <td klm="uraian_prog_keg">PROGRAM PENGELOLAAN SUMBER DAYA AIR (SDA)</td>
-                                <td klm="satuan">
-                                    <div contenteditable=""></div>
-                                </td>
-                                <td klm="indikator">
-                                    <div contenteditable=""></div>
-                                </td>
-                                <td klm="data_capaian_awal">
-                                    <div contenteditable="" rms="" onkeypress="return rumus(event);">0,00</div>
-                                </td>
-                                <td klm="kondisi_akhir">0,00</td>
-                                <td klm="jumlah">141.804.961.217,00</td>
-                                <td class="center aligned">
-                                    <div class="ui icon basic mini buttons"><button class="ui red button" name="del_row"
-                                            jns="edit" tbl="renstra" id_row="132"><i
-                                                class="trash alternate outline red icon"></i></button></div>
-                                </td>
-                            </tr>
-                            <tr id_row="133">
-                                <td klm="kd_sub_keg">1.3.02.2.01</td>
-                                <td klm="uraian_prog_keg">Pengelolaan SDA dan Bangunan Pengaman Pantai pada Wilayah
-                                    Sungai (WS) dalam 1 (Satu) Daerah Kabupaten/Kota</td>
-                                <td klm="satuan">
-                                    <div contenteditable=""></div>
-                                </td>
-                                <td klm="indikator">
-                                    <div contenteditable=""></div>
-                                </td>
-                                <td klm="data_capaian_awal">
-                                    <div contenteditable="" rms="" onkeypress="return rumus(event);">0,00</div>
-                                </td>
-                                <td klm="kondisi_akhir">0,00</td>
-                                <td klm="jumlah">40.123.461.217,00</td>
-                                <td class="center aligned">
-                                    <div class="ui icon basic mini buttons"><button class="ui red button" name="del_row"
-                                            jns="edit" tbl="renstra" id_row="133"><i
-                                                class="trash alternate outline red icon"></i></button></div>
-                                </td>
-                            </tr>
-                            <tr id_row="136">
-                                <td klm="kd_sub_keg">1.3.02.2.01.0093</td>
-                                <td klm="uraian_prog_keg">Normalisasi/Restorasi Sungai</td>
-                                <td klm="satuan">
-                                    <div contenteditable="">km</div>
-                                </td>
-                                <td klm="indikator">
-                                    <div contenteditable="">Panjang Sungai yang Dinormalisasi/Direstorasi</div>
-                                </td>
-                                <td klm="data_capaian_awal">
-                                    <div contenteditable="" rms="" onkeypress="return rumus(event);">0,00</div>
-                                </td>
-                                <td klm="kondisi_akhir">325.590,07</td>
-                                <td klm="jumlah">24.419.255.000,00</td>
-                                <td class="center aligned">
-                                    <div class="ui icon basic mini buttons"> <button class="ui button" name="flyout"
-                                            jns="edit" tbl="renstra" id_row="136"><i
-                                                class="edit outline blue icon"></i></button><button
-                                            class="ui red button" name="del_row" jns="edit" tbl="renstra"
-                                            id_row="136"><i class="trash alternate outline red icon"></i></button></div>
-                                </td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th class="right aligned" colspan="22">
-                                    <div class="ui center pagination menu"><a class="disabled icon item"><i
-                                                class="angle left icon"></i></a><a class="active item"
-                                            tbl="renstra">1</a><a class="item" name="page" hal="2" ret="go"
-                                            tbl="renstra">2</a><a class="item" name="page" hal="3" ret="go"
-                                            tbl="renstra">3</a><a class="item" name="page" hal="19" ret="go"
-                                            tbl="renstra"><i class="angle double right chevron icon"></i></a><a
-                                            class="icon item" name="page" hal="1" ret="next" tbl="renstra"><i
-                                                class="angle right icon"></i></a></div>
-                                </th>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </div>
+
+            <h3 class="ui dividing header">Data Renstra</h3>
+
+            <div id="renstra-table-container" class="ui long scrolling fluid container">
+                <!-- tabel akan dimuat di sini via AJAX -->
             </div>
         </div>
     </div>
 </div>
+
+<!-- Modal reusable (bisa dibedakan per tbl nanti) -->
+<div id="modal-renstra" class="ui modal">
+    <div class="header">Form Renstra</div>
+    <div class="content scrolling">
+        <form id="form-renstra" class="ui form">
+            @csrf
+            <input type="hidden" name="id">
+            <input type="hidden" name="tbl"> <!-- renstra atau tujuan_sasaran_renstra -->
+
+            <!-- field dasar (akan diubah via JS sesuai tbl) -->
+            <div class="field">
+                <label>Uraian</label>
+                <textarea name="uraian" required></textarea>
+            </div>
+
+            <button type="submit" class="ui positive button">Simpan</button>
+        </form>
+    </div>
+</div>
 @endsection
+
+@push('scripts')
+<script>
+$(function() {
+    const container = $('#renstra-table-container');
+
+    function loadTable(tbl = 'renstra', page = 1) {
+        $.ajax({
+            url: '{{ route("anggaran.renstra.table") }}',
+            data: { tbl: tbl, page: page },
+            success: function(res) {
+                if (res.success) {
+                    container.html(res.data.html);
+
+                    // update tombol action sesuai tbl aktif
+                    $('.ui.right.floated.basic.icon.buttons button').attr('tbl', tbl);
+
+                    // handle klik pagination
+                    container.find('[name="page"]').on('click', function(e) {
+                        e.preventDefault();
+                        const hal = $(this).attr('hal');
+                        loadTable(tbl, hal);
+                    });
+
+                    // update total anggaran (khusus renstra)
+                    if (tbl === 'renstra' && res.data.total) {
+                        $('[name="total-anggaran"]').text('Rp ' + res.data.total);
+                    }
+                }
+            }
+        });
+    }
+
+    // klik tab menu
+    $('.ui.secondary.vertical.pointing.menu .item').on('click', function() {
+        $('.ui.secondary.vertical.pointing.menu .item').removeClass('active');
+        $(this).addClass('active');
+
+        const tbl = $(this).attr('tbl');
+        loadTable(tbl);
+    });
+
+    // load awal
+    loadTable('renstra');
+
+    // tombol tambah
+    $('body').on('click', '[jns="add"]', function() {
+        const tbl = $(this).attr('tbl');
+        $('#form-renstra')[0].reset();
+        $('#form-renstra [name="tbl"]').val(tbl);
+        $('#modal-renstra .header').text('Tambah ' + (tbl === 'renstra' ? 'Renstra' : 'Tujuan & Sasaran'));
+        $('#modal-renstra').modal('show');
+    });
+
+    // submit form (contoh sederhana)
+    $('#form-renstra').on('submit', function(e) {
+        e.preventDefault();
+        alert('Implementasi simpan ' + $('#form-renstra [name="tbl"]').val() + ' di sini');
+        $('#modal-renstra').modal('hide');
+        // refresh tabel aktif
+        const tblAktif = $('.ui.secondary.vertical.pointing.menu .active.item').attr('tbl');
+        loadTable(tblAktif);
+    });
+});
+</script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     // Anggaran + sub-menu
     Route::prefix('anggaran')->name('anggaran.')->group(function () {
-        Route::get('/renstra',        [AnggaranController::class, 'renstra'])        ->name('renstra');
-        Route::get('/renja-dpa',      [AnggaranController::class, 'renjaDpa'])      ->name('renja-dpa');
-        Route::get('/renja-perubahan',[AnggaranController::class, 'renjaPerubahan'])->name('renja-perubahan');
-        Route::get('/dppa',           [AnggaranController::class, 'dppa'])           ->name('dppa');
+        Route::get('/renstra',        [AnggaranController::class, 'renstra'])->name('renstra');
+        // ← tambahkan baris ini (untuk load tabel via AJAX)
+        Route::get('/renstra/table',  [AnggaranController::class, 'renstraTable'])->name('renstra.table');
+        Route::get('/renja-dpa',      [AnggaranController::class, 'renjaDpa'])->name('renja-dpa');
+        Route::get('/renja-perubahan', [AnggaranController::class, 'renjaPerubahan'])->name('renja-perubahan');
+        Route::get('/dppa',           [AnggaranController::class, 'dppa'])->name('dppa');
     });
 
     // Kontrak
@@ -42,52 +44,52 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     // Realisasi + sub-menu
     Route::prefix('realisasi')->name('realisasi.')->group(function () {
-        Route::get('/input',    [RealisasiController::class, 'input'])   ->name('input');
-        Route::get('/spj',      [RealisasiController::class, 'spj'])     ->name('spj');
-        Route::get('/laporan',  [RealisasiController::class, 'laporan']) ->name('laporan');
+        Route::get('/input',    [RealisasiController::class, 'input'])->name('input');
+        Route::get('/spj',      [RealisasiController::class, 'spj'])->name('spj');
+        Route::get('/laporan',  [RealisasiController::class, 'laporan'])->name('laporan');
     });
 
     // Referensi + sub-menu (banyak sekali)
     Route::prefix('referensi')->name('referensi.')->group(function () {
-        Route::get('/bidang-urusan', [ReferensiController::class, 'bidangUrusan']) ->name('bidang-urusan');
-        Route::get('/program',       [ReferensiController::class, 'program'])      ->name('program');
-        Route::get('/kegiatan',      [ReferensiController::class, 'kegiatan'])     ->name('kegiatan');
-        Route::get('/sub-kegiatan',  [ReferensiController::class, 'subKegiatan'])  ->name('sub-kegiatan');
-        Route::get('/rekanan',       [ReferensiController::class, 'rekanan'])      ->name('rekanan');
-        Route::get('/satuan',        [ReferensiController::class, 'satuan'])       ->name('satuan');
-        Route::get('/mapping',       [ReferensiController::class, 'mapping'])      ->name('mapping');
-        Route::get('/neraca',        [ReferensiController::class, 'neraca'])       ->name('neraca');
-        Route::get('/akun',          [ReferensiController::class, 'akun'])         ->name('akun');
-        Route::get('/sumber-dana',   [ReferensiController::class, 'sumberDana'])   ->name('sumber-dana');
-        Route::get('/organisasi',    [ReferensiController::class, 'organisasi'])   ->name('organisasi');
-        Route::get('/peraturan',     [ReferensiController::class, 'peraturan'])    ->name('peraturan');
-        Route::get('/wilayah',       [ReferensiController::class, 'wilayah'])      ->name('wilayah');
+        Route::get('/bidang-urusan', [ReferensiController::class, 'bidangUrusan'])->name('bidang-urusan');
+        Route::get('/program',       [ReferensiController::class, 'program'])->name('program');
+        Route::get('/kegiatan',      [ReferensiController::class, 'kegiatan'])->name('kegiatan');
+        Route::get('/sub-kegiatan',  [ReferensiController::class, 'subKegiatan'])->name('sub-kegiatan');
+        Route::get('/rekanan',       [ReferensiController::class, 'rekanan'])->name('rekanan');
+        Route::get('/satuan',        [ReferensiController::class, 'satuan'])->name('satuan');
+        Route::get('/mapping',       [ReferensiController::class, 'mapping'])->name('mapping');
+        Route::get('/neraca',        [ReferensiController::class, 'neraca'])->name('neraca');
+        Route::get('/akun',          [ReferensiController::class, 'akun'])->name('akun');
+        Route::get('/sumber-dana',   [ReferensiController::class, 'sumberDana'])->name('sumber-dana');
+        Route::get('/organisasi',    [ReferensiController::class, 'organisasi'])->name('organisasi');
+        Route::get('/peraturan',     [ReferensiController::class, 'peraturan'])->name('peraturan');
+        Route::get('/wilayah',       [ReferensiController::class, 'wilayah'])->name('wilayah');
     });
 
     // Standar Harga Satuan
     Route::prefix('standar-harga')->name('standar-harga.')->group(function () {
-        Route::get('/ssh',  [StandarHargaController::class, 'ssh'])  ->name('ssh');
-        Route::get('/hspk', [StandarHargaController::class, 'hspk']) ->name('hspk');
-        Route::get('/asb',  [StandarHargaController::class, 'asb'])  ->name('asb');
-        Route::get('/sbu',  [StandarHargaController::class, 'sbu'])  ->name('sbu');
+        Route::get('/ssh',  [StandarHargaController::class, 'ssh'])->name('ssh');
+        Route::get('/hspk', [StandarHargaController::class, 'hspk'])->name('hspk');
+        Route::get('/asb',  [StandarHargaController::class, 'asb'])->name('asb');
+        Route::get('/sbu',  [StandarHargaController::class, 'sbu'])->name('sbu');
     });
 
     // Kepegawaian
     Route::prefix('kepegawaian')->name('kepegawaian.')->group(function () {
-        Route::get('/asn',              [KepegawaianController::class, 'asn'])              ->name('asn');
-        Route::get('/surat-keputusan',  [KepegawaianController::class, 'suratKeputusan'])  ->name('surat-keputusan');
-        Route::get('/register-surat',   [KepegawaianController::class, 'registerSurat'])   ->name('register-surat');
-        Route::get('/tata-naskah',      [KepegawaianController::class, 'tataNaskah'])      ->name('tata-naskah');
+        Route::get('/asn',              [KepegawaianController::class, 'asn'])->name('asn');
+        Route::get('/surat-keputusan',  [KepegawaianController::class, 'suratKeputusan'])->name('surat-keputusan');
+        Route::get('/register-surat',   [KepegawaianController::class, 'registerSurat'])->name('register-surat');
+        Route::get('/tata-naskah',      [KepegawaianController::class, 'tataNaskah'])->name('tata-naskah');
     });
 
     // Pesan, Pengaturan, Profil
-    Route::get('/pesan',       [PesanController::class, 'index'])      ->name('pesan.index');
-    Route::get('/pengaturan',  [PengaturanController::class, 'index']) ->name('pengaturan.index');
+    Route::get('/pesan',       [PesanController::class, 'index'])->name('pesan.index');
+    Route::get('/pengaturan',  [PengaturanController::class, 'index'])->name('pengaturan.index');
 
     // Profile (dari Breeze, dipertahankan)
-    Route::get('/profile',       [ProfileController::class, 'edit'])   ->name('profile.edit');
+    Route::get('/profile',       [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile',     [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile',    [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
